b68ab7a28a1100f83fd20ad1e54ec2ca27d8739f
Howard Wang <idontwannarock@gmail.com>
2023-09-26 17:18:51 +0800

    feat: prevent default password from logging in console

--- src/main/java/com/example/lab/spring/reactive/web/Application.java
+++ src/main/java/com/example/lab/spring/reactive/web/Application.java
@@ -2,9 +2,10 @@ package com.example.lab.spring.reactive.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import reactor.core.publisher.Hooks;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ReactiveUserDetailsServiceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
